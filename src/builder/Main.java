ee56674efec8576b6c35ddc591fe6abb0b8d0193
Serdar Ayalp <serdarayalp@googlemail.com>
2021-01-26 22:54:47 +0100

    Kleine Änderung

--- src/builder/Main.java
+++ src/builder/Main.java
@@ -4,15 +4,6 @@ import singleton.MySingleton;
 
 public class Main {
     public static void main(String[] args) {
-
-        MySingleton object1 = MySingleton.createObject();
-        MySingleton object2 = MySingleton.createObject();
-
-        if (object1 == object2) {
-            System.out.println("beide Objekte sind gleich...");
-        }
-
-
         // Cake cake = new Cake.Builder(500, 500).butter(200).chocolate(300).build();
         Cake.CakeBuilder cakeBuilder = new Cake.CakeBuilder(500, 500);
         // Optional
